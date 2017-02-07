48867954cd2b998bfdea219849af86e5a9db2fc2
suilulu <m13869841793@gmail.com>
2017-02-07 21:52:17 +0800

    implement destroy all products

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,12 @@ Rails.application.routes.draw do
   resources :products do
     member do
       post :add_to_cart
+
     end
   end
-  resources :carts
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
 end
